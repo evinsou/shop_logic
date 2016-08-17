9d8c31463aa61b77050a3d58363d74a54bf15245
Ivan <evinsou@gmail.com>
2016-08-17 12:53:30 +0300

    Add checkout and spec

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,55 +1,39 @@
-require "byebug"
 require 'spec_helper'
-require_relative '../lib/product.rb'
 
 module Shop
 	class Checkout
 
+    attr_reader :item_list, :rule_list
+
 		def initialize(rules)
 			@item_list = []
-			@rules = rules
+			@rule_list = rules
 		end
 
 		def scan(product_name)
-			if current_product = find_by_name(product_name)
-				current_product.amount += 1
-			else
-				item_list << Shop::Product.new(product_name)
-			end
+      if product = find_by_name(product_name)
+        product.increase_amount
+      else
+        item_list << Shop::Product.new(product_name)
+      end
 		end
-						
+
 		def total
-			product_sum = {}
-
-			item_list.each do |item|
-				if rule = get_rule(item.name, item.amount)
-					product_price = (rule.new_price || item.price)
-
-					total = rule.formula(item.amount, product_price)
-				else
-					total = item.price * item.amount
-				end
-				product_sum[item.name] = total
-			end
-			sum = 0
-
-			product_sum.values.each { |i| sum += i }
-
-			sum
+      collect_product_sums[:total]
 		end
 
 		private
 
-		def item_list
-			@item_list
-		end
+    def find_by_name(product_name)
+      item_list.detect { |item| item.name == product_name }
+    end
 
-		def find_by_name(product_name)
-			item_list.detect { |item| item.name == product_name }
-		end
-
-		def get_rule(product_name, number)
-			@rules.detect { |rule| rule.applies?(product_name, number) }
-		end
+    def collect_product_sums
+      calc = Shop::CheckoutCalculator.new(item_list, rule_list)
+      product_sum = calc.yield_sum_by_product
+      total = product_sum.values.inject { |sum, part_sum| sum + part_sum }
+      product_sum[:total] = total
+      product_sum
+    end
 	end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,54 +1,42 @@
 require 'spec_helper'
-require_relative '../lib/checkout.rb'
-require_relative '../lib/rule.rb'
 
 describe Shop::Checkout do
-	context 'some context' do
-		let(:items) { ['FR', 'SR', 'CF', 'AJ'] }
-		let(:rules) { [] }
-		let(:checkout) { Shop::Checkout.new(rules) }
-		before do
-			items.each { |item| checkout.scan(item) }
-		end
+	let(:items) { ['FR', 'SR', 'CF', 'AJ'] }
+	let(:rules) { [] }
+	let(:checkout) { Shop::Checkout.new(rules) }
 
-		it 'returns simple price and number when no rule is apply' do
-			expect(checkout.total).to eq 26.59
-		end
+	before { items.each { |item| checkout.scan(item) } }
 
-		context 'when one for free and descount rules present' do
-			let(:rules) { [Shop::Rule.new('SR', 3, 0, 4.5), Shop::Rule.new('FR', 1, 1)] }
+	it 'returns simple price and number when no rule is apply' do
+		expect(checkout.total).to eq 26.59
+	end
 
-			context 'when one fruit tea is free' do
-				let(:items) { ['FR', 'SR', 'FR', 'FR', 'CF'] }
+	context 'when one for free and descount rules present' do
+		let(:rules) { [Shop::Rule.new('SR', 3, 0, 4.5),
+                    Shop::Rule.new('FR', 1, 1)] }
 
-				it 'gets correct sum' do
-					expect(checkout.total).to eq 22.45
-				end
-			end
+		context 'when one fruit tea is free' do
+			let(:items) { ['FR', 'SR', 'FR', 'FR', 'CF'] }
 
-			context 'when one for free rule' do
-				let(:items) { ['FR', 'FR'] }
-
-				it 'gets correct sum' do
-					expect(checkout.total).to eq 3.11
-				end
-			end
-
-			context 'when discount for strawberry' do
-				let(:items) { ['SR', 'SR', 'FR', 'SR'] }
-
-				it 'gets correct sum' do
-					expect(checkout.total).to eq 16.61
-				end
+			it 'gets correct sum' do
+				expect(checkout.total).to eq 22.45
 			end
 		end
 
-		it 'returns another sum'
+		context 'when one for free rule' do
+			let(:items) { ['FR', 'FR'] }
 
-		it 'parses when time of purchse, time by day and by day of the week'
+			it 'gets correct sum' do
+				expect(checkout.total).to eq 3.11
+			end
+		end
 
-		it 'parses when two different in the list'
+		context 'for discount for strawberry' do
+			let(:items) { ['SR', 'SR', 'FR', 'SR'] }
 
-		it 'returns a list like check with product and number of it'
+			it 'gets correct sum' do
+				expect(checkout.total).to eq 16.61
+			end
+		end
 	end
 end
