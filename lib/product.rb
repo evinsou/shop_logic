e3a259ab6ce16aed051fc494c55506cc39e9b78f
Ivan <evinsou@gmail.com>
2016-08-17 12:51:50 +0300

    Add product with spec

--- lib/product.rb
+++ lib/product.rb
@@ -1,25 +1,18 @@
 module Shop
 	class Product
-		attr_accessor :name
+		attr_accessor :name, :amount
+    attr_reader :price
 
-		PRICES = { 'FR' => 3.11, 'SR' => 5.00, 'CF' => 11.23, 'AJ' => 7.25}
+		PRICES = { 'FR' => 3.11, 'SR' => 5.00, 'CF' => 11.23, 'AJ' => 7.25 }
 
-		def initialize(name, amount = 1)
+		def initialize(name)
 			@name = name
-      @amount = amount
+      @amount = 1
 			@price = PRICES[name]
 		end
 
-    def price
-      @price
-    end
-
-    def amount
-      @amount
-    end
-
-    def amount=(new_value)
-      @amount = new_value
+    def increase_amount
+      self.amount += 1
     end
 	end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
-require_relative '../lib/product.rb'
 
 describe Shop::Product do
-	context 'some context' do
-		let(:product) { Shop::Product.new('apple') }
 
-		it 'returns correct name' do
-			expect(product.name).to eq 'apple'
-		end
+	it 'returns a correct product' do
+    product = Shop::Product.new('FR')
+		expect(product.name).to eq('FR')
+    expect(product.price).to eq(3.11)
+    expect(product.amount).to eq(1)
 	end
+
+  it 'returns a correct product' do
+    product = Shop::Product.new('SR')
+    expect(product.name).to eq('SR')
+    expect(product.price).to eq(5.0)
+    expect(product.amount).to eq(1)
+  end
+
+  it 'returns a correct product' do
+    product = Shop::Product.new('CF')
+    expect(product.name).to eq('CF')
+    expect(product.price).to eq(11.23)
+    expect(product.amount).to eq(1)
+  end
+
+  it 'returns a correct product' do
+    product = Shop::Product.new('AJ')
+    expect(product.name).to eq('AJ')
+    expect(product.price).to eq(7.25)
+    expect(product.amount).to eq(1)
+  end
 end
